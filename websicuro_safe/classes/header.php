157fc8b80f14282c9fc1a2aa5b2a4d87a7668290
Davide Bosco <davide.bosco917@edu.unito.it>
2026-03-19 01:01:39 +0100

    Delete websicuro_safe directory

--- websicuro_safe/classes/header.php
+++
@@ -1,8 +0,0 @@
-<?php ob_start();?>
-<?php
-// Protezione contro clickjacking
-header("X-Frame-Options: DENY");
-
-// Ancora più sicuro: CSP per frame
-header("Content-Security-Policy: frame-ancestors 'none';");
-?>
